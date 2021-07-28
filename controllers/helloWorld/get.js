3198ea5c3831c6ea94c1100d6ec36bb6875484c9
Mactto <shc9928@gmail.com>
2021-07-29 00:14:43 +0900

    [BE] 수정 :: 테스트 채팅 코드 추가 (세환)

--- controllers/helloWorld/get.js
+++ controllers/helloWorld/get.js
@@ -2,6 +2,35 @@ const { User } = require('../../models');
 const { Chat } = require('../../models');
 
 module.exports = async (req, res) => {
+  const chatSample = [
+    {
+        message: "안녕 오랑이",
+        room_id: 7,
+        user_id: 12345678
+    },
+    {
+      message: "가나다라마바사",
+      room_id: 7,
+      user_id: 32432123
+    },
+    {
+      message: "node.js",
+      room_id: 7,
+      user_id: 12345678
+    },
+    {
+      message: "오랑우톡",
+      room_id: 7,
+      user_id: 32432123
+    }
+];
+  for (var index in chatSample) {
+    await Chat.create({
+        message: chatSample[index].message,
+        room_id:chatSample[index].room_id,
+        user_id: chatSample[index].user_id
+    })
+  }
 
   
         // 테스트 유저 생성 코드
