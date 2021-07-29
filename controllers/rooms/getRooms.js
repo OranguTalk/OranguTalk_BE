b4bc8184aa030ec69822c71cfe368cf8639e650a
mactto <shc9928@gmail.com>
2021-07-29 13:56:01 +0900

    [BE] 로그인 된 유저 채팅방 조회 API 참가자 프로필 추가 (세환)

--- controllers/rooms/getRooms.js
+++ controllers/rooms/getRooms.js
@@ -1,4 +1,4 @@
-const { Room, Participant } = require("../../models");
+const { User, Room, Participant } = require("../../models");
 
 module.exports = async (req, res) => {
     const participant = await Participant.findAll({
@@ -6,9 +6,16 @@ module.exports = async (req, res) => {
             user_id: req.authData.dataValues['user_id']
         }
     });
-    data = [];
+    const data = new Array();
+    const avatars = new Array();
     for (const p of participant) {
         const roomInfo = await Room.findOne({where: {id: p.dataValues['room_id']}});
+        const user_info = await Participant.findAll({where: {room_id: p.dataValues['room_id']}});
+        for (const user_id of user_info) {
+            const avatar = await User.findOne({where: {user_id: user_id.dataValues['user_id']}});
+            avatars.push(avatar.dataValues['profileImage']);
+        }
+        roomInfo.dataValues.avatars = avatars;
         data.push(await roomInfo.dataValues);
     }
     res.status(200).json({data});
